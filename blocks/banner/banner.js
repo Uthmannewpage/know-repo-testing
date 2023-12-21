ec715b3fb9258c4b3139052820fd58c44fb7abda
Lucia <lucia.tiseanu@newpge.io>
2023-12-21 21:20:02 +0200

    update

--- blocks/banner/banner.js
+++ blocks/banner/banner.js
@@ -1,30 +1,20 @@
 export default function decorate(block) {
-  const parentBlock = block.closest(".section");
-  const parentBlockStyle = block.closest(".section").style;
+  const parentBlock = block.closest('.section');
+  const parentBlockStyle = block.closest('.section').style;
   const blockStyle = block.style;
   const metaData = parentBlock.dataset;
+
   const {
-    backgroundColor,
-    backgroundHide,
-    textColor,
-    underlinedInclinedTextColor,
-    textAlignVertical,
-    imageAlign,
-    paddingTop,
-    paddingBottom,
-    marginTop,
-    marginBottom,
-    imageCover,
-  } = metaData;
+    backgroundColor, backgroundHide, textColor, underlinedInclinedTextColor, textAlignVertical, imageAlign, paddingTop, paddingBottom, marginTop, marginBottom, imageCover } = metaData;
   const [richTextEl, pictureEl] = [...block.children];
 
   if (backgroundColor) parentBlockStyle.backgroundColor = backgroundColor;
   if (textColor) blockStyle.color = textColor;
   if (underlinedInclinedTextColor) {
-    block.querySelectorAll("em u").forEach((element) => {
+    block.querySelectorAll('em u').forEach((element) => {
       element.style.color = underlinedInclinedTextColor;
-      element.style.fontStyle = "normal";
-      element.style.textDecoration = "none";
+      element.style.fontStyle = 'normal';
+      element.style.textDecoration = 'none';
     });
   }
   if (paddingTop) blockStyle.paddingTop = `${paddingTop}rem`;
@@ -34,19 +24,19 @@ export default function decorate(block) {
 
   if (backgroundHide) parentBlock.classList.add(`hide-${backgroundHide}`);
 
-  if (imageCover && imageCover === "small") {
+  if (imageCover && imageCover === 'small') {
     blockStyle.background = `url(${
-      pictureEl.querySelector("img").getAttribute("src").split("?")[0]
-    }) no-repeat 0 0 / cover ${backgroundColor || "#000"}`;
+      pictureEl.querySelector('img').getAttribute('src').split('?')[0]
+    }) no-repeat 0 0 / cover ${backgroundColor || '#000'}`;
 
     block.innerHTML = `
-            <div class="container-fluid">
-                <div class="row d-none d-lg-flex">
-                    <div class="col-5 ps-4">${richTextEl.innerHTML}</div>
+            <div class='container-fluid'>
+                <div class='row d-none d-lg-flex'>
+                    <div class='col-5 ps-4'>${richTextEl.innerHTML}</div>
                 </div>
-                <div class="row d-lg-none justify-content-center">
-                    <div class="col-12 col-md-7 text-center">${richTextEl.innerHTML}</div>
-                    <div class="col-12 p-0 text-center bck-img">
+                <div class='row d-lg-none justify-content-center'>
+                    <div class='col-12 col-md-7 text-center'>${richTextEl.innerHTML}</div>
+                    <div class='col-12 p-0 text-center bck-img'>
                     ${pictureEl.innerHTML}
                     </div>
                 </div>
@@ -54,47 +44,47 @@ export default function decorate(block) {
         `;
   } else if (imageCover) {
     parentBlockStyle.background = `url(${
-      pictureEl.querySelector("img").getAttribute("src").split("?")[0]
-    }) no-repeat top center / 100% ${backgroundColor || "#000"}`;
+      pictureEl.querySelector('img').getAttribute('src').split('?')[0]
+    }) no-repeat top center / 100% ${backgroundColor || '#000'}`;
 
-    if (imageCover === "full-left") {
+    if (imageCover === 'full-left') {
       parentBlockStyle.background = `url(${
-        pictureEl.querySelector("img").getAttribute("src").split("?")[0]
-      }) no-repeat top left / auto 100% ${backgroundColor || "#000"}`;
-    } else if (imageCover === "full-center") {
+        pictureEl.querySelector('img').getAttribute('src').split('?')[0]
+      }) no-repeat top left / auto 100% ${backgroundColor || '#000'}`;
+    } else if (imageCover === 'full-center') {
       parentBlockStyle.background = `url(${
-        pictureEl.querySelector("img").getAttribute("src").split("?")[0]
-      }) no-repeat top center / auto 100% ${backgroundColor || "#000"}`;
-    } else if (imageCover === "full-right") {
+        pictureEl.querySelector('img').getAttribute('src').split('?')[0]
+      }) no-repeat top center / auto 100% ${backgroundColor || '#000'}`;
+    } else if (imageCover === 'full-right') {
       parentBlockStyle.background = `url(${
-        pictureEl.querySelector("img").getAttribute("src").split("?")[0]
-      }) no-repeat top right / auto 100% ${backgroundColor || "#000"}`;
+        pictureEl.querySelector('img').getAttribute('src').split('?')[0]
+      }) no-repeat top right / auto 100% ${backgroundColor || '#000'}`;
     }
 
     block.innerHTML = `
-            <div class="container-fluid">
-                <div class="row d-none d-lg-flex">
-                    <div class="col-12 col-md-7">${richTextEl.innerHTML}</div>
+            <div class='container-fluid'>
+                <div class='row d-none d-lg-flex'>
+                    <div class='col-12 col-md-7'>${richTextEl.innerHTML}</div>
                 </div>
-                <div class="row d-lg-none justify-content-center">
-                    <div class="col-12 col-md-7 text-center">${richTextEl.innerHTML}</div>
+                <div class='row d-lg-none justify-content-center'>
+                    <div class='col-12 col-md-7 text-center'>${richTextEl.innerHTML}</div>
                 </div>
             </div>
         `;
   } else {
     block.innerHTML = `
-            <div class="container-fluid">
-                <div class="row d-none d-lg-flex">
-                    <div class="col-5 ps-4">${richTextEl.innerHTML}</div>
-                    <div class="col-7 img-right bck-img">
+            <div class='container-fluid'>
+                <div class='row d-none d-lg-flex'>
+                    <div class='col-5 ps-4'>${richTextEl.innerHTML}</div>
+                    <div class='col-7 img-right bck-img'>
                     ${pictureEl.innerHTML}
                     </div>
                 </div>
-                <div class="row d-lg-none justify-content-center">
-                    <div class="col-12 p-0 text-center bck-img">
+                <div class='row d-lg-none justify-content-center'>
+                    <div class='col-12 p-0 text-center bck-img'>
                     ${pictureEl.innerHTML}
                     </div>
-                    <div class="col-12 col-md-7 text-center">${richTextEl.innerHTML}</div>
+                    <div class='col-12 col-md-7 text-center'>${richTextEl.innerHTML}</div>
                 </div>
             </div>
         `;
@@ -102,11 +92,11 @@ export default function decorate(block) {
 
   if (textAlignVertical) {
     block
-      .querySelector(".row")
+      .querySelector('.row')
       .classList.add(`align-items-${textAlignVertical}`);
   }
 
   if (imageAlign) {
-    block.querySelector(".img-right").style.textAlign = imageAlign;
+    block.querySelector('.img-right').style.textAlign = imageAlign;
   }
 }
