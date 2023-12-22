68d63dda49f9e28bc97eb08079bd66a56b210e85
reshail2 <reshail.muzammil@pfizer.com>
2023-12-22 19:33:44 +0500

    UpdateContentBlock

---
+++ blocks/MediaText/mediatext.js
@@ -0,0 +1,49 @@
+function updateContentBlock(mediaType, mediaSrc, textContent, textPosition) {
+  const mediaContainer = document.getElementById('content-media');
+  const textContainer = document.getElementById('content-text');
+
+  // Clear existing content
+  mediaContainer.innerHTML = '';
+  textContainer.innerHTML = '';
+
+  // Set the media content
+  if (mediaType === 'image') {
+    const image = document.createElement('img');
+    image.src = mediaSrc;
+    mediaContainer.appendChild(image);
+  } else if (mediaType === 'video') {
+    const video = document.createElement('video');
+    video.src = mediaSrc;
+    video.controls = true;
+    mediaContainer.appendChild(video);
+  }
+
+  // Set the text content
+  textContainer.textContent = textContent;
+
+  // Set the text position
+  if (textPosition === 'left') {
+    textContainer.style.order = -1;
+  } else {
+    textContainer.style.order = 1;
+  }
+}
+
+// Example usage:
+
+export default function decorate(block) {
+  const {
+    type, imagePath, text, position,
+  } = block.dataset;
+  updateContentBlock(
+    type,
+    imagePath,
+    text,
+    position,
+  );
+  //   [...block.children].forEach((row) => {
+  //     [...row.children].forEach((col) => {
+
+  //     });
+  //   });
+}
