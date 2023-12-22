6de5973d5ae4c733107fea743a5222fff6ef5416
Lucia <lucia.tiseanu@newpge.io>
2023-12-22 15:05:48 +0200

    Banner Homapge + general settings

--- blocks/banner/banner.js
+++ blocks/banner/banner.js
@@ -1,103 +1,43 @@
 export default function decorate(block) {
-  const parentBlock = block.closest('.section');
-  const parentBlockStyle = block.closest('.section').style;
-  const blockStyle = block.style;
-  const metaData = parentBlock.dataset;
-
-  const {
-    backgroundColor, backgroundHide, textColor, underlinedInclinedTextColor, textAlignVertical, imageAlign, paddingTop, paddingBottom, marginTop, marginBottom, imageCover,
-  } = metaData;
-  const [richTextEl, pictureEl] = [...block.children];
-
-  if (backgroundColor) parentBlockStyle.backgroundColor = backgroundColor;
-  if (textColor) blockStyle.color = textColor;
-  if (underlinedInclinedTextColor) {
-    block.querySelectorAll('em u').forEach((element) => {
-      element.style.color = underlinedInclinedTextColor;
-      element.style.fontStyle = 'normal';
-      element.style.textDecoration = 'none';
-    });
-  }
-  if (paddingTop) blockStyle.paddingTop = `${paddingTop}rem`;
-  if (paddingBottom) blockStyle.paddingBottom = `${paddingBottom}rem`;
-  if (marginTop) blockStyle.marginTop = `${marginTop}rem`;
-  if (marginBottom) blockStyle.marginBottom = `${marginBottom}rem`;
-
-  if (backgroundHide) parentBlock.classList.add(`hide-${backgroundHide}`);
-
-  if (imageCover && imageCover === 'small') {
-    blockStyle.background = `url(${
-      pictureEl.querySelector('img').getAttribute('src').split('?')[0]
-    }) no-repeat 0 0 / cover ${backgroundColor || '#000'}`;
+    const parentBlock = block.closest('.section');
+    const parentBlockStyle = block.closest('.section').style;
+    const blockStyle = block.style;
+    const metaData = parentBlock.dataset;
+    const {
+      backgroundColor, backgroundHide, textColor, underlinedInclinedTextColor, textAlignVertical, imageAlign, paddingTop, paddingBottom, marginTop, marginBottom, imageCover,
+    } = metaData;
+    const bannerItems = [...block.children];
 
     block.innerHTML = `
-            <div class='container-fluid'>
-                <div class='row d-none d-lg-flex'>
-                    <div class='col-5 ps-4'>${richTextEl.innerHTML}</div>
-                </div>
-                <div class='row d-lg-none justify-content-center'>
-                    <div class='col-12 col-md-7 text-center'>${richTextEl.innerHTML}</div>
-                    <div class='col-12 p-0 text-center bck-img'>
-                    ${pictureEl.innerHTML}
-                    </div>
-                </div>
+  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+      ${bannerItems.length && bannerItems.map((_, index) => `
+        <li data-target="#myCarousel" data-slide-to="${index}" ${index === 0 ? 'class="active"' : ''}></li>
+      `).join('')}
+    </ol>
+    <div class="carousel-inner">
+      ${bannerItems.length && bannerItems.map((slide, index) => {
+        const [content, image] = [...slide.children];
+        return `
+        <div class="carousel-item ${index === 0 ? 'active' : ''}">
+            <img class="d-block w-100" src="${image.querySelector('img').getAttribute('src').split('?')[0]}" alt="Slide">
+            <div class="carousel-caption d-none d-md-block rows col-xs-12 col-sm-7 col-md-5 col-lg-5">
+              ${content.innerHTML}
             </div>
-        `;
-  } else if (imageCover) {
-    parentBlockStyle.background = `url(${
-      pictureEl.querySelector('img').getAttribute('src').split('?')[0]
-    }) no-repeat top center / 100% ${backgroundColor || '#000'}`;
+          </div>`;
+      }).join('')}
+    </div>
+    <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>`;
 
-    if (imageCover === 'full-left') {
-      parentBlockStyle.background = `url(${
-        pictureEl.querySelector('img').getAttribute('src').split('?')[0]
-      }) no-repeat top left / auto 100% ${backgroundColor || '#000'}`;
-    } else if (imageCover === 'full-center') {
-      parentBlockStyle.background = `url(${
-        pictureEl.querySelector('img').getAttribute('src').split('?')[0]
-      }) no-repeat top center / auto 100% ${backgroundColor || '#000'}`;
-    } else if (imageCover === 'full-right') {
-      parentBlockStyle.background = `url(${
-        pictureEl.querySelector('img').getAttribute('src').split('?')[0]
-      }) no-repeat top right / auto 100% ${backgroundColor || '#000'}`;
-    }
+  
+   // blockStyle.background = `url(${pictureEl.querySelector('img').getAttribute('src').split('?')[0]}) no-repeat 0 0 / cover ${backgroundColor || '#000'}`;
 
-    block.innerHTML = `
-            <div class='container-fluid'>
-                <div class='row d-none d-lg-flex'>
-                    <div class='col-12 col-md-7'>${richTextEl.innerHTML}</div>
-                </div>
-                <div class='row d-lg-none justify-content-center'>
-                    <div class='col-12 col-md-7 text-center'>${richTextEl.innerHTML}</div>
-                </div>
-            </div>
-        `;
-  } else {
-    block.innerHTML = `
-            <div class='container-fluid'>
-                <div class='row d-none d-lg-flex'>
-                    <div class='col-5 ps-4'>${richTextEl.innerHTML}</div>
-                    <div class='col-7 img-right bck-img'>
-                    ${pictureEl.innerHTML}
-                    </div>
-                </div>
-                <div class='row d-lg-none justify-content-center'>
-                    <div class='col-12 p-0 text-center bck-img'>
-                    ${pictureEl.innerHTML}
-                    </div>
-                    <div class='col-12 col-md-7 text-center'>${richTextEl.innerHTML}</div>
-                </div>
-            </div>
-        `;
   }
-
-  if (textAlignVertical) {
-    block
-      .querySelector('.row')
-      .classList.add(`align-items-${textAlignVertical}`);
-  }
-
-  if (imageAlign) {
-    block.querySelector('.img-right').style.textAlign = imageAlign;
-  }
-}
