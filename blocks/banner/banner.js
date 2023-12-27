d6e4a0406c76ceecf8964b29394b535373e8c132
Lucia <lucia.tiseanu@newpge.io>
2023-12-27 23:46:41 +0200

    updates

--- blocks/banner/banner.js
+++ blocks/banner/banner.js
@@ -5,41 +5,43 @@ export default function decorate(block) {
   const tables = block.querySelectorAll('table');
   const parentBlock = block.closest('.section');
   const metaData = parentBlock.dataset;
-  const { type, video, imageCover, titleColor } = metaData
+  const {
+    type, video, imageCover, titleColor,
+  } = metaData;
   const bannerItems = [...block.children];
 
-  if (titleColor) block.querySelector('h1').style.color = titleColor
+  if (titleColor) block.querySelector('h1').style.color = titleColor;
 
   if (type === 'carousel') {
     block.innerHTML = `
-      <div id="myCarousel" class="carousel slide" data-ride="carousel">
-        <ol class="carousel-indicators">
-          ${bannerItems.length && bannerItems.map((_, index) => `
-            <li data-target="#myCarousel" data-slide-to="${index}" ${index === 0 ? 'class="active"' : ''}></li>
-          `).join('')}
-        </ol>
-        <div class="carousel-inner">
-        ${bannerItems.length && bannerItems.map((slide, index) => {
-      const [content, image] = [...slide.children];
-      return `<div class="carousel-item ${index === 0 ? 'active' : ''}">
-          <img class="d-block w-100" src="${image.querySelector('img').getAttribute('src').split('?')[0]}" alt="Slide">
-          <div class="carousel-caption d-none d-md-block rows col-xs-12 col-sm-7 col-md-5 col-lg-5">
-            ${content.innerHTML}
-          </div>
-        </div>`;
-    }).join('')}
-      </div>
-        <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="sr-only">Previous</span>
-        </a>
-        <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="sr-only">Next</span>
-        </a>
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+      <ol class="carousel-indicators">
+        ${bannerItems.length && bannerItems.map((_, index) => `
+          <li data-target="#myCarousel" data-slide-to="${index}" ${index === 0 ? 'class="active"' : ''}></li>
+        `).join('')}
+      </ol>
+      <div class="carousel-inner">
+      ${bannerItems.length && bannerItems.map((slide, index) => {
+    const [content, image] = [...slide.children];
+    return `<div class="carousel-item ${index === 0 ? 'active' : ''}">
+        <img class="d-block w-100" src="${image.querySelector('img').getAttribute('src').split('?')[0]}" alt="Slide">
+        <div class="carousel-caption d-none d-md-block rows col-xs-12 col-sm-7 col-md-5 col-lg-5">
+          ${content.innerHTML}
+        </div>
       </div>`;
+  }).join('')}
+    </div>
+      <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div>`;
   } else {
-    const [ contentLeft, contentRight, contentImage] = bannerItems;
+    const [contentLeft, contentRight, contentImage] = bannerItems;
     if (imageCover === 'background') {
       block.style.background = `url(${contentImage.querySelector('img').getAttribute('src').split('?')[0]}) no-repeat top / cover transparent`;
     }
@@ -59,14 +61,14 @@ export default function decorate(block) {
     }
 
     // searchng for tables:
-    tables.forEach(table => {
-        const firstRow = table.querySelector('tr:first-child');
-        const firstRowContent = firstRow.innerText.trim();
+    tables.forEach((table) => {
+      const firstRow = table.querySelector('tr:first-child');
+      const firstRowContent = firstRow.innerText.trim();
 
-        if (allPossibleTables.includes(firstRowContent)) {
-          table.classList.add(firstRowContent);
-          firstRow.style.display = 'none';
-        }
+      if (allPossibleTables.includes(firstRowContent)) {
+        table.classList.add(firstRowContent);
+        firstRow.style.display = 'none';
+      }
     });
 
     block.innerHTML = `
