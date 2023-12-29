b4d99fe798d512ed8cd43f885de1606b45ea04f2
Faizan Sarwer <faizansarwer@Faizans-MacBook-Pro.local>
2023-12-29 18:44:19 +0530

    style css moved to head html

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -40,7 +40,7 @@ export default async function decorate(block) {
     <link rel="stylesheet" defer async href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
   
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" defer async integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="/styles/styles.css"/>`;
+   `;
   footer.append(filesDiv);
   block.append(footer);
 }
