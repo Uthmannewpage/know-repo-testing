c474d00b14dab3ae93bfdfab71b0363c0c5ed4ad
Faizan Sarwer <faizansarwer@Faizans-MacBook-Pro.local>
2023-12-29 18:41:58 +0530

    bootstrap css moved back to head html

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -38,7 +38,7 @@ export default async function decorate(block) {
     <link href="https://fonts.googleapis.com/css2?family=Lato:wght@100;300;400;700;900&display=swap" defer async rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@300;400;500;900&display=swap" defer async rel="stylesheet">
     <link rel="stylesheet" defer async href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" defer async rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" defer async integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="/styles/styles.css"/>`;
   footer.append(filesDiv);
