deba05065d1daf7594439d873b14f123151990f1
Faizan Sarwer <faizansarwer@Faizans-MacBook-Pro.local>
2023-12-29 18:37:30 +0530

    js and css files loaded from footer

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -33,6 +33,14 @@ export default async function decorate(block) {
   selectElement.addEventListener('change', () => {
     // const selectedValue = this.value;
   });
-
+  const filesDiv = document.createElement('div');
+  filesDiv.innerHTML = `
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@100;300;400;700;900&display=swap" defer async rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@300;400;500;900&display=swap" defer async rel="stylesheet">
+    <link rel="stylesheet" defer async href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" defer async rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" defer async integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="/styles/styles.css"/>`;
+  footer.append(filesDiv);
   block.append(footer);
 }
