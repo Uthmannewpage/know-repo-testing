c2dd17831f83866bb36ab1f7454cd7802ec46ce1
namith-scindhia-newpage-io <namith.scindhia@newpage.io>
2024-01-03 20:35:19 +0530

    Fixes for review comments.

--- blocks/how-it-works/how-it-works.js
+++ blocks/how-it-works/how-it-works.js
@@ -1,55 +1,40 @@
-import { createOptimizedPicture } from '../../scripts/aem.js';
-
 export default function decorate(block) {
   block.classList.add('d-flex', 'flex-column', 'flex-md-row');
 
-  [...block.children].forEach((row, index) => {
-    row.classList.add(
-      'd-flex',
-      'flex-column',
-      'align-items-center',
-      'how-it-works-column',
-      `column-${index + 1}`,
-    );
-    const children = [...row.children];
-    const pictureDiv = children[0];
+  const rows = [...block.children];
+  block.innerHTML = rows.map((row, index) => {
+    const [image, content] = [...row.children];
+    const buttonContainer = content.querySelector('.button-container');
+    if (buttonContainer) {
+      buttonContainer.children[0].classList.remove('button');
 
-    const iconContainer = document.createElement('div');
-    iconContainer.classList.add('how-it-works-icon', 'd-flex');
+      const triangle1 = document.createElement('span');
+      triangle1.classList.add('triangle', 'ms-2', 'me-1');
 
-    pictureDiv
-      .querySelectorAll('img')
-      .forEach((img) => iconContainer.append(createOptimizedPicture(img.src, img.alt, false, [{ width: '110' }])));
+      const triangle2 = document.createElement('span');
+      triangle2.classList.add('triangle');
 
-    const numericalDiv = document.createElement('div');
-    numericalDiv.classList.add('how-it-works-number', 'my-4');
-    numericalDiv.innerHTML = index + 1;
+      buttonContainer.append(triangle1);
+      buttonContainer.append(triangle2);
+    }
+    const colItem = `
+    <div class="d-flex flex-column align-items-center how-it-works-column column-${index + 1}">
+      <div class="how-it-works-column-wrapper">
+        <div class="d-flex flex-column align-items-center how-it-works-image-conatiner">
+          <div class="d-flex how-it-works-icon>
+            ${image.innerHTML}
+          </div>
+          <div class="how-it-works-number my-4">
+            ${index + 1}
+          </div>
+        </div>
+        <div class="how-it-works-content">
+          ${content.innerHTML}
+        </div>
+      </div>
+    </div>
+    `;
 
-    const imagesContainer = document.createElement('div');
-    imagesContainer.classList.add(
-      'how-it-works-image-conatiner',
-      'flex-column',
-      'd-flex',
-      'align-items-center',
-    );
-
-    imagesContainer.append(iconContainer);
-    imagesContainer.append(numericalDiv);
-
-    const columnWrapper = document.createElement('div');
-    columnWrapper.classList.add('how-it-works-column-wrapper');
-    columnWrapper.append(imagesContainer);
-
-    columnWrapper.append(imagesContainer);
-
-    const textContentDiv = document.createElement('div');
-    const textContentP = document.createElement('p');
-    textContentP.innerHTML = children[1].innerHTML;
-    textContentDiv.append(textContentP);
-    textContentDiv.classList.add('how-it-works-content');
-
-    columnWrapper.append(textContentDiv);
-
-    row.innerHTML = columnWrapper.outerHTML;
+    return colItem;
   });
 }
