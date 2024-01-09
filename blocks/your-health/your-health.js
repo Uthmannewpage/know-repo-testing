47cbb5b0de76ea5507931bfe719863d8a1ac1124
reshail2 <reshail.muzammil@pfizer.com>
2024-01-09 15:18:44 +0500

    Updating due to generic style changes

--- blocks/your-health/your-health.js
+++ blocks/your-health/your-health.js
@@ -9,27 +9,43 @@ export default function decorate(block) {
   rightColumn.className = 'col-md-6 col-sm-12 bg-grey';
   block.classList.add('row');
 
-  block.appendChild(leftColumn);
-  leftColumn.append(...contentLeft.childNodes);
-  const isMobile = window.innerWidth < 600;
-  let imgLink = 'https://www.knowpneumonia.com/images/shape-new-arrow.png';
-  if (isMobile) {
-    imgLink = 'https://www.knowpneumonia.com/images/whiteArrow-test1.png';
-  }
-  if (contentRight) {
-    block.appendChild(rightColumn);
-    rightColumn.innerHTML = `
-      <div class='row'>
-      <div class='col-md-4 col-sm-0 p-0'> <img  style='height: ${isMobile ? 'auto' : '40rem'}' src='${imgLink}' /></div>
-      <div class='col-md-8 col-sm-12 '> ${contentRight.innerHTML} </div>
-      </div>
-    `;
-  }
+  // Clear the block's inner HTML to avoid duplication
+  block.innerHTML = '';
 
+  // Append columns to the block
+  block.appendChild(leftColumn);
+  block.appendChild(rightColumn);
+
+  // Append children of contentLeft to leftColumn
+  leftColumn.append(...contentLeft.childNodes);
+
+  // Function to set image and right column content
+  const setImage = () => {
+    const isMobile = window.innerWidth < 768;
+    let imgLink = 'https://www.knowpneumonia.com/images/shape-new-arrow.png';
+    if (isMobile) {
+      imgLink = 'https://www.knowpneumonia.com/images/whiteArrow-test1.png';
+    }
+
+    if (contentRight) {
+      rightColumn.innerHTML = `
+        <div class='row' style='height: 100%'>
+          <div class='col-md-4 col-sm-0 p-0'> 
+            <img style='height: ${isMobile ? 'auto' : '100%'}' src='${imgLink}' />
+          </div>
+          <div class='col-md-8 col-sm-12 '> ${contentRight.innerHTML} </div>
+        </div>
+      `;
+    }
+  };
+
+  // Initialize and set resize event listener
+  setImage();
+  window.addEventListener('resize', setImage);
+
+  // Style h2 elements
   block.querySelectorAll('h2').forEach((it) => {
     it.style.color = '#783384';
+    it.style.fontSize = '28px';
   });
-  // emptying out innerHTML
-  contentLeft.innerHTML = '';
-  contentRight.innerHTML = '';
 }
